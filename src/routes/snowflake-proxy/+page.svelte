58b6b2b78a94072596364540396f94fbb8be9767
jonathan <github.com.starship007@passmail.net>
2026-03-18 18:50:22 +0100

    create snowflake-proxy page

---
+++ src/routes/snowflake-proxy/+page.svelte
@@ -0,0 +1,71 @@
+<script>
+	import AsciiPrint from '$lib/components/ascii-print.svelte';
+	import * as Navbar from '$lib/components/navbar/index';
+	import SnowflakeProxy from '$lib/components/snowflake-proxy.svelte';
+
+	const banner = `
+           .                      +++          
+           *=   .+               :*-           
+            ++: +=            ..+*:            
+             =+++        ..  .*++   ....       
+              :++        ==  =+*-              
+               =++  -    =++++++  -*:     .    
+                +=***   :+*+*+*  :+=     +=    
+              .-:*++.     - ..+.++=      +.    
+               = *+.=     :....-=       -*.  .-
+      .        +....:.   -.:..=.       =**++=  
+  .. .++        +-..:. . ::..:      .=++:.     
+   ==+*=        .::..:...:: ....-+++**.        
+      .****++....::..:::::..::.:.=***=+*+=:-=-=
+    .:=+*+-.:.............::.=..         ..  -+
+**+=+=++*=+++=-.:-::......::.: .    .-        -
+:           -++==:.:.:.....:...=-++-           
+              .=....:..:......:.-              
+          *==+:-:..::..::.:.....=.             
+            +*=.:.:::...::.....:.**+.          
+            +:...-=::*::-=:.:....*=            
+           =+==:  ++==*. :-.:= ++*             
+     :. - ++=    :*-.=-:==**=   +*=            
+     .: :++     --.      -*+.  :.+*            
+     :: -+    -  .         +.     ++           
+      :=--*+=                   :+*+=          
+   -+-+.:                      -   .*:         
+    :+  :                            +..:      
+   .*        ..                      .+.       
+             .=.                      +        
+                                      -        
+`;
+
+	let completed = $state(false);
+</script>
+
+<Navbar.Main />
+
+<main class="container max-w-3xl py-28 md:m-auto">
+	<div class="gap-x-10 md:flex md:flex-row">
+		<header>
+			<AsciiPrint
+				class="md:h-109"
+				text={banner}
+				oncomplete={() => (completed = true)}
+				chunkSize={24}
+			/>
+		</header>
+
+		{#if completed}
+			<div>
+				<h1 class="mt-12 font-mono text-xs! leading-3 -tracking-widest md:mt-0">
+					battle
+					<span class="ml-10">censorship</span>
+				</h1>
+				<br />
+				<p class="max-w-lg text-muted-foreground md:max-w-none">
+					Enable snowflake proxy and leave it on to donate bandwith and help tor users bypass
+					censorship and access internet freely.
+				</p>
+
+				<SnowflakeProxy class="mt-20" />
+			</div>
+		{/if}
+	</div>
+</main>
---
+++ src/routes/snowflake-proxy/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+	return {
+		meta: {
+			title: 'dottmp | snowflake proxy',
+			description:
+				'Enable snowflake proxy and leave it on to donate bandwith and help tor users bypass censorship and access internet freely.'
+		}
+	};
+};
