ec84a88ff80d07851aa5c38a9985a7a95723989c
jonathan <github.com.starship007@passmail.net>
2026-03-18 18:52:39 +0100

    add mode toggle component

---
+++ src/lib/components/mode-toggle.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { toggleMode, mode } from 'mode-watcher';
+</script>
+
+<button class="btn btn-text w-29.5 text-start sm:w-26" onclick={toggleMode}>
+	m: mode ~ <span>{mode.current}</span>
+</button>
