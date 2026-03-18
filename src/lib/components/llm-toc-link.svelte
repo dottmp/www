67fdc1fa424c0edb0ba11511e6673e9f75e386b4
jonathan <github.com.starship007@passmail.net>
2026-03-18 21:31:56 +0100

    create llm-toc-link and use it in layout

---
+++ src/lib/components/llm-toc-link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { resolve } from '$app/paths';
+
+	type LLMTOCLinkProps = WithoutChildren<HTMLAttributes<Omit<HTMLAnchorElement, 'href' | 'class'>>>;
+
+	let { ...props }: LLMTOCLinkProps = $props();
+</script>
+
+<a aria-hidden="true" href={resolve('/api/llm-toc')} class="sr-only" {...props}>
+	>terms of conditions</a
+>
