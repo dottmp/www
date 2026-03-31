d71ad7c595ce1b0f4e1fde51e5cc17c33f89aecd
Jonathan <github.com.starship007@passmail.net>
2026-03-31 19:43:42 +0000

    Refactor/misc styling (#17)
    * add transition to time and increase font weight
    
    * increase spacing
    
    * decrease spacing

--- src/lib/components/article.svelte
+++ src/lib/components/article.svelte
@@ -12,10 +12,10 @@
 	let { article, class: classname, tabindex, ...props }: ArticleProps = $props();
 </script>
 
-<article tabindex="-1" class={cn('group relative outline-hidden', classname)} {...props}>
+<article tabindex="-1" class={cn('group relative outline-hidden  ', classname)} {...props}>
 	<div class="inline-flex items-center gap-2">
 		<h2
-			class="line-clamp-1 text-muted-foreground transition group-focus-within:text-foreground group-hover:text-foreground"
+			class="line-clamp-1 font-medium text-muted-foreground transition group-focus-within:text-foreground group-hover:text-foreground"
 		>
 			<a {tabindex} class="outline-hidden" href={resolve(`/articles/${article.slug}`)}>
 				<span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6"></span>
@@ -23,7 +23,9 @@
 			</a>
 		</h2>
 
-		<time class="text-sm leading-0 text-nowrap text-muted-foreground sm:text-xs">
+		<time
+			class="text-sm leading-0 text-nowrap text-muted-foreground transition group-hover:text-foreground sm:text-xs"
+		>
 			{formatDate(new Date(article.date))}
 		</time>
 	</div>
