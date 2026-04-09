44bb01a6a6aed67f0777b6e6ac2643b55e424cfe
jonathan <github.com.starship007@passmail.net>
2026-04-09 12:50:33 +0200

    remove sveltekite in memory rate limiter

--- src/hook.server.ts
+++ src/hook.server.ts
@@ -1,29 +1,7 @@
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
-import { RetryAfterRateLimiter } from 'sveltekit-rate-limiter/server';
 
 import { dev } from '$app/environment';
-
-export const limiter = new RetryAfterRateLimiter({
-	IP: [1000, 'd'],
-	IPUA: [100, 'm']
-});
-
-const handleRateLimit: Handle = async ({ event, resolve }) => {
-	const status = await limiter.check(event);
-	if (status.limited) {
-		const response = new Response(
-			`You are being rate limited. Please try after ${status.retryAfter} seconds.`,
-			{
-				status: 429,
-				headers: { 'Retry-After': status.retryAfter.toString() }
-			}
-		);
-		return response;
-	}
-	const response = await resolve(event);
-	return response;
-};
 
 const handleHeaders: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
@@ -62,4 +40,4 @@ const handleHeaders: Handle = async ({ event, resolve }) => {
 	return response;
 };
 
-export const handle = sequence(handleRateLimit, handleHeaders);
+export const handle = sequence(handleHeaders);
